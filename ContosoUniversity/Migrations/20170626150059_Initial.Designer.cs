2bd0106218833d8a6bcd8ffd554e104cac2d62be
Henry Huang <Henry.Huang@dimensional.com>
2017-06-29 09:15:51 -0500

    port to Mac; dev tool VS Studio/dotnet cli; db: sqlite/mysql/mssql; add unit tests

---
+++ ContosoUniversity/Migrations/20170626150059_Initial.Designer.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ContosoUniversity.Data;
+
+namespace ContosoUniversity.Migrations
+{
+    [DbContext(typeof(SchoolContext))]
+    [Migration("20170626150059_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2");
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Course", b =>
+                {
+                    b.Property<int>("CourseID");
+
+                    b.Property<int>("Credits");
+
+                    b.Property<int>("DepartmentID");
+
+                    b.Property<string>("Title")
+                        .HasMaxLength(50);
+
+                    b.HasKey("CourseID");
+
+                    b.HasIndex("DepartmentID");
+
+                    b.ToTable("Course");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.CourseAssignment", b =>
+                {
+                    b.Property<int>("CourseID");
+
+                    b.Property<int>("InstructorID");
+
+                    b.HasKey("CourseID", "InstructorID");
+
+                    b.HasIndex("InstructorID");
+
+                    b.ToTable("CourseAssignment");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Department", b =>
+                {
+                    b.Property<int>("DepartmentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<decimal>("Budget");
+
+                    b.Property<int?>("InstructorID");
+
+                    b.Property<string>("Name")
+                        .HasMaxLength(50);
+
+                    b.Property<byte[]>("RowVersion")
+                        .IsConcurrencyToken()
+                        .ValueGeneratedOnAddOrUpdate();
+
+                    b.Property<DateTime>("StartDate");
+
+                    b.HasKey("DepartmentID");
+
+                    b.HasIndex("InstructorID");
+
+                    b.ToTable("Department");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Enrollment", b =>
+                {
+                    b.Property<int>("EnrollmentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("CourseID");
+
+                    b.Property<int?>("Grade");
+
+                    b.Property<int>("StudentID");
+
+                    b.HasKey("EnrollmentID");
+
+                    b.HasIndex("CourseID");
+
+                    b.HasIndex("StudentID");
+
+                    b.ToTable("Enrollment");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.OfficeAssignment", b =>
+                {
+                    b.Property<int>("InstructorID");
+
+                    b.Property<string>("Location")
+                        .HasMaxLength(50);
+
+                    b.HasKey("InstructorID");
+
+                    b.ToTable("OfficeAssignment");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Person", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Discriminator")
+                        .IsRequired();
+
+                    b.Property<string>("FirstMidName")
+                        .IsRequired()
+                        .HasColumnName("FirstName")
+                        .HasMaxLength(50);
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasMaxLength(50);
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Person");
+
+                    b.HasDiscriminator<string>("Discriminator").HasValue("Person");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Instructor", b =>
+                {
+                    b.HasBaseType("ContosoUniversity.Data.Models.Person");
+
+                    b.Property<DateTime>("HireDate");
+
+                    b.ToTable("Instructor");
+
+                    b.HasDiscriminator().HasValue("Instructor");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Student", b =>
+                {
+                    b.HasBaseType("ContosoUniversity.Data.Models.Person");
+
+                    b.Property<DateTime>("EnrollmentDate");
+
+                    b.ToTable("Student");
+
+                    b.HasDiscriminator().HasValue("Student");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Course", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Models.Department", "Department")
+                        .WithMany("Courses")
+                        .HasForeignKey("DepartmentID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.CourseAssignment", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Models.Course", "Course")
+                        .WithMany("CourseAssignments")
+                        .HasForeignKey("CourseID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("ContosoUniversity.Data.Models.Instructor", "Instructor")
+                        .WithMany("CourseAssignments")
+                        .HasForeignKey("InstructorID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Department", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Models.Instructor", "Administrator")
+                        .WithMany()
+                        .HasForeignKey("InstructorID");
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.Enrollment", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Models.Course", "Course")
+                        .WithMany("Enrollments")
+                        .HasForeignKey("CourseID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("ContosoUniversity.Data.Models.Student", "Student")
+                        .WithMany("Enrollments")
+                        .HasForeignKey("StudentID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("ContosoUniversity.Data.Models.OfficeAssignment", b =>
+                {
+                    b.HasOne("ContosoUniversity.Data.Models.Instructor", "Instructor")
+                        .WithOne("OfficeAssignment")
+                        .HasForeignKey("ContosoUniversity.Data.Models.OfficeAssignment", "InstructorID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ ContosoUniversityTests/Controllers/StudentsControllerTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using static ContosoUniversity.Data.DbInitializer;
+using Xunit;
+using ContosoUniversity.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ContosoUniversity.Data.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversityTests.Controllers
+{
+    public class StudentsControllerTests
+    {
+		private SchoolContext _context;
+
+        public StudentsControllerTests()
+        {
+            setup();
+        }
+
+        void setup() {
+            var builder = new DbContextOptionsBuilder<SchoolContext>().UseInMemoryDatabase();
+			_context = new SchoolContext(builder.Options);
+
+            DbInitializer.Initialize(_context);
+        }
+
+        [Fact]
+        public void TestQueryStudents() {
+			var students = from s in _context.Students select s;
+
+            var student = students.Where(s => s.LastName.Contains("Alexander")).Single();
+
+            Assert.Equal(student.LastName, "Alexander");
+
+		}
+
+	    [Fact]
+	    public async Task TestIndexAsync() {
+	        var studentController = new StudentsController(_context);
+	        var res = await studentController.Index("name_desc", null, null, null);
+
+	        var viewResult = Assert.IsType<ViewResult>(res);
+
+	        var model = Assert.IsAssignableFrom<IEnumerable<Student>>(viewResult.ViewData.Model);
+
+			Assert.Equal(3, model.Count());
+
+		    var student = model.First();
+            //System.Console.WriteLine(student.LastName);
+		    Assert.Equal(student.LastName, "Olivetto");
+		}
+
+		[Fact]
+		public void TestIndex()
+		{
+			var studentController = new StudentsController(_context);
+			var task = studentController.Index("name_desc", null, null, null);
+
+            var res = task.Result;
+			var viewResult = Assert.IsType<ViewResult>(res);
+
+			var model = Assert.IsAssignableFrom<IEnumerable<Student>>(viewResult.ViewData.Model);
+
+			Assert.Equal(3, model.Count());
+
+			var student = model.First();
+			//System.Console.WriteLine(student.LastName);
+			Assert.Equal(student.LastName, "Olivetto");
+		}
+    }
+}
